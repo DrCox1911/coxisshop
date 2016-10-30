b61cd43d21eccc29d65fb521543bc653c832f561
DrCox1911 <michael.hochriegl@googlemail.com>
2016-10-30 12:54:12 +0100

    Version 1.2
    New version with definable settings
    It also is depended of the CoxisUtils mod now, which handles the ini-file reading

--- CoxisShop/media/lua/client/CoxisShop.lua
+++ CoxisShop/media/lua/client/CoxisShop.lua
@@ -4,10 +4,11 @@
 #	@author: 	Dr_Cox1911					                                                            #
 #	@notes:		Many thanks to RJ´s LastStand code and all the other modders out there					#
 #	@notes:		For usage instructions check forum link below                                  			#
-#	@link: 													       										#
+#	@link: 		https://theindiestone.com/forums/index.php?/topic/20228-coxis-shop/											       										#
 #########################################################################################################
 --]]
 
+require 'CoxisUtil'
 
 CoxisShop = {};
 CoxisShop.modData = {};
@@ -15,8 +16,14 @@ CoxisShop.modData = {};
 CoxisShop.boostGoldLvl = {};
 CoxisShop.boostXPLvl = {};
 CoxisShop.upgradeScreen = {};
-CoxisShop.debug = true;
+CoxisShop.settings = {};
+CoxisShop.gamemode = nil;
+CoxisShop.debug = false;
 
+
+-- **************************************************************************************
+-- loading up the saved player money from moddata
+-- **************************************************************************************
 CoxisShop.InitPlayer = function()
 	--if isClient() then
 		for i = 0,getNumActivePlayers() - 1 do
@@ -28,21 +35,21 @@ CoxisShop.InitPlayer = function()
 				CoxisShop.modData[i].CoxisShopBoostGoldLevel = CoxisShop.modData[i].CoxisShopBoostGoldLevel or 1;
 				CoxisShop.modData[i].CoxisShopBoostXpLevel = CoxisShop.modData[i].CoxisShopBoostXpLevel or 1;
 				CoxisShop.modData[i].CoxisShopStartingGoldLevel = CoxisShop.modData[i].CoxisShopStartingGoldLevel or 1;
-				print("...CoxisShop...")
-				print(CoxisShop.modData[i].playerMoney);
-				print("...CoxisShop...")
 			end
 		end
 	--end
 end
 
+
+-- **************************************************************************************
+-- showing the shop window (pretty much a copy from RJs code)
+-- **************************************************************************************
 local function showUpgradeScreen(_playerNum)
-	print("here in show screen")
 	if not CoxisShop.upgradeScreen[_playerNum] then
 		local x = getPlayerScreenLeft(_playerNum)
 		local y = getPlayerScreenTop(_playerNum)
 		
-		CoxisShop.upgradeScreen[_playerNum] = ISCoxisShop:new(x+70,y+50,420,408,_playerNum)
+		CoxisShop.upgradeScreen[_playerNum] = ISCoxisShop:new(x+70,y+50,420,408,_playerNum, CoxisShop.settings)
 		--CoxisShop.upgradeScreen[_playerNum] = ISCoxisShopUpgradeTab:new(x+70,y+50,320,608,_playerNum)
 		CoxisShop.upgradeScreen[_playerNum]:initialise()
 		CoxisShop.upgradeScreen[_playerNum]:addToUIManager()
@@ -65,52 +72,100 @@ local function showUpgradeScreen(_playerNum)
 	end
 end
 
-CoxisShop.onKeyPressed = function(_key)
---~ 		if key == getCore():getKey("Equip/Unequip Handweapon") then
---~ 			if CoxisShop.zombiesSpawned > 0 then
---~ 				CoxisShop.onZombieDead();
---~ 			end
---~ 		end
 
+-- **************************************************************************************
+-- asking the server to send the settings like amount of money, items in shop, ...
+-- **************************************************************************************
+CoxisShop.AskSettings = function(_ticks)
+	if _ticks > 200 then
+		print("now asking for settings");
+		Events.OnTick.Remove(CoxisShop.AskSettings);
+		CoxisShop.SendToServer("askSettings", CoxisShop.settings);
+	end
+end
+
+
+-- **************************************************************************************
+-- handling key presses here
+-- **************************************************************************************
+CoxisShop.onKeyPressed = function(_key)
 		if _key == Keyboard.KEY_O then
 			if getSpecificPlayer(0) and not getSpecificPlayer(0):isDead() then
 				showUpgradeScreen(0)
 			end
 		elseif _key == Keyboard.KEY_L then
 			if getSpecificPlayer(0) and not getSpecificPlayer(0):isDead() and CoxisShop.debug then
+				-- CoxisShop.AskSettings();
 				getSpecificPlayer(0):getModData().playerMoney = math.floor(getSpecificPlayer(0):getModData().playerMoney + 500);
 			end
 		end
 end
 
+
+-- **************************************************************************************
+-- basic function to send something to the server
+-- the _data has to be a table, _event a unique name that is also recognized by the server
+-- **************************************************************************************
+CoxisShop.SendToServer = function(_event, _data)
+	print("...CoxisShop...SENDING DATA TO SERVER")
+	sendClientCommand('CoxisShop', _event, _data);
+	print("...CoxisShop...SENDING DATA TO SERVER DONE")
+end
+
+
+-- **************************************************************************************
+-- basic function to receive something from the server
+-- all the _command from the server have to be known here
+-- **************************************************************************************
 CoxisShop.ReceiveServerCommand = function(_module, _command, _args)
 	if _module ~= 'CoxisShop' then return end
 	print("...CoxisShop...RECEIVED SERVER COMMAND");
 	if _command == 'afterZombieDead' then
 		CoxisShop.AfterZombieDead();
 	end
-
+	if _command == "sendSettings" then
+		CoxisShop.settings = _args;
+		print(CoxisShop.settings["WEAPONS"]["Base.Axe"]);
+	end
+	CoxisShop.conn = true;
 end
 
+
+-- **************************************************************************************
+-- giving the player the money
+-- the info if zombie is dead is transmitted by the server (see ReceiveServerCommand)
+-- **************************************************************************************
 CoxisShop.AfterZombieDead = function()
 	print("...CoxisShop...ZOMBIE DIED")
 		for i = 0,getNumActivePlayers() - 1 do
 			local playerObj = getSpecificPlayer(i)
 			if playerObj then
 	--~ 		getSpecificPlayer(i):getModData()["CoxisShopXp"] = getSpecificPlayer(i):getModData()["CoxisShopXp"] + 10;
-				CoxisShop.modData[i].playerMoney = math.floor(CoxisShop.modData[i].playerMoney + (10 + (10 * (tonumber(playerObj:getModData()["CoxisShopBoostGoldLevel"]) / 100))));
-				print(CoxisShop.modData[i].playerMoney)
+				randi = ZombRand(100);
+				if (randi >= tonumber(CoxisShop.settings["BASIC"]["random"])) then	-- if _settings["random"]=0 than the player will receive the money every kill, else it´s random, the lower the random-number the likelier
+					CoxisShop.modData[i].playerMoney = math.floor(CoxisShop.modData[i].playerMoney + (tonumber(CoxisShop.settings["BASIC"]["amount"]) + (10 * (tonumber(playerObj:getModData()["CoxisShopBoostGoldLevel"]) / 100))));
+				end
 			end
 		end
-
 end
 
+-- **************************************************************************************
+-- loading the settings if mode is SP, if MP than the settings are received from the server
+-- **************************************************************************************
+CoxisShop.LoadSettings = function()
+	CoxisShop.settings = CoxisUtil.readINI("CoxisShop", "CoxisShopSettings.ini");
+end
+
+-- **************************************************************************************
+-- init the client, registering events and whatnot
+-- **************************************************************************************
 CoxisShop.Init = function()
 	if isClient() then
 		print("...CoxisShop...INIT CLIENT")
 		Events.OnKeyPressed.Add(CoxisShop.onKeyPressed);
-		Events.OnServerCommand.Add(CoxisShop.ReceiveServerCommand);
+		Events.OnTick.Add(CoxisShop.AskSettings);
 		CoxisShop.InitPlayer();
+		CoxisShop.gamemode = "MP";
 		print("...CoxisShop...INIT CLIENT DONE")
 	end
 	
@@ -119,9 +174,11 @@ CoxisShop.Init = function()
 		Events.OnKeyPressed.Add(CoxisShop.onKeyPressed);
 		Events.OnZombieDead.Add(CoxisShop.AfterZombieDead);
 		CoxisShop.InitPlayer();
+		CoxisShop.LoadSettings();
+		CoxisShop.gamemode = "SP";
 		print("...CoxisShop...INIT SP DONE")
 	end
 end
 
 Events.OnGameStart.Add(CoxisShop.Init)
---Events.OnServerStarted.Add(CoxisShop.InitServer)
+Events.OnServerCommand.Add(CoxisShop.ReceiveServerCommand);
--- CoxisShop/media/lua/server/CoxisShopServer.lua
+++ CoxisShop/media/lua/server/CoxisShopServer.lua
@@ -4,17 +4,33 @@
 #	@author: 	Dr_Cox1911					                                                            #
 #	@notes:		Many thanks to RJ´s LastStand code and all the other modders out there					#
 #	@notes:		For usage instructions check forum link below                                  			#
-#	@link: 													       										#
+#	@link: 		https://theindiestone.com/forums/index.php?/topic/20228-coxis-shop/											       										#
 #########################################################################################################
 --]]
 
-CoxisShopServer = {};
+require 'CoxisUtil'
 
+CoxisShopServer = {};
+CoxisShopServer.settings = {};
+
+-- **************************************************************************************
+-- sending the info that zombie is dead to client
+-- zombies are server-sided since a couple of builds, so the event isn´t fired on the client any longer
+-- **************************************************************************************
 CoxisShopServer.AfterZombieDead = function()
-	print("Here in onZombieDead")
 	CoxisShopServer.SendToClient("afterZombieDead", {})
 end
 
+
+CoxisShopServer.SendSettings = function()
+	CoxisShopServer.SendToClient("sendSettings", CoxisShopServer.settings)
+end
+
+
+-- **************************************************************************************
+-- basic function to send something to the client
+-- the _data has to be a table, _event a unique name that is also recognized by the client
+-- **************************************************************************************
 CoxisShopServer.SendToClient = function(_event, _data)
 	print("...CoxisShop...SENDING DATA TO CLIENT")
 	sendServerCommand('CoxisShop', _event, _data);
@@ -22,14 +38,39 @@ CoxisShopServer.SendToClient = function(_event, _data)
 end
 
 
+CoxisShopServer.ReceiveFromClient = function(_module, _command, _player, _args)
+	if _module ~= 'CoxisShop' then return end
+	print("...CoxisShop...received from client!")
+	if _command == 'askSettings' then
+		print("New Client knocked, sending the settings...");
+		CoxisShopServer.SendSettings();
+		print("Settings sended");
+	end
+end
+
+
+-- **************************************************************************************
+-- reading in ini-file
+-- can be used to read any setting shipped with the mod
+-- **************************************************************************************
+CoxisShopServer.LoadSettings = function()
+	CoxisShopServer.settings = CoxisUtil.readINI("CoxisShop", "CoxisShopSettings.ini");
+end
+
+
+-- **************************************************************************************
+-- init the server, registering events and whatnot
+-- **************************************************************************************
 CoxisShopServer.InitServer = function()
 	if (isServer()) then
 		print("...CoxisShop...INIT SERVER")
+		CoxisShopServer.LoadSettings();
 		Events.OnZombieDead.Add(CoxisShopServer.AfterZombieDead);
+		Events.OnClientCommand.Add(CoxisShopServer.ReceiveFromClient);
 		print("...CoxisShop...INIT SERVER DONE")
 	end
 
 end
 
 Events.OnServerStarted.Add(CoxisShopServer.InitServer)
-Events.OnGameStart.Add(CoxisShopServer.InitServer)
+--Events.OnGameStart.Add(CoxisShopServer.InitServer)
